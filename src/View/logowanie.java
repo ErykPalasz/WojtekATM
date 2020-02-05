05f12e88e246ccd640cd1cab70d1eec842931c3c
blanko124 <wojtek.rybacki2@onet.pl>
2020-02-05 18:06:47 +0100

    Widoki fix2

--- src/View/logowanie.java
+++ src/View/logowanie.java
@@ -1,6 +1,21 @@
 package View;
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class logowanie {
-    private void createUIComponents() {
+    private JPanel panel;  // nie widze nazwy obiektu
+    private JButton operuj;
+    private JTextField login;
+    private JPasswordField haslo;
+
+
+    public logowanie() {
+        operuj.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
     }
 }
